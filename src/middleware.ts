0b6152cbf7ff57f433b24585df607a2cc77f06b4
Deepak <deepak2603dk@gmail.com>
2024-03-01 05:27:47 +0530

    auth middleware

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+    const path = request.nextUrl.pathname;
+    const isPublicPath = path === '/auth/login' || path === '/auth/signup';
+    const token = request.cookies.get("token")?.value || "";
+
+    if (isPublicPath && token) {
+        return NextResponse.redirect(new URL('/home', request.nextUrl.origin).toString());
+    }
+
+    if (!isPublicPath && !token) {
+        return NextResponse.redirect(new URL('/auth/login', request.nextUrl.origin).toString());
+    }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    '/auth/:path*',
+    '/',
+    '/home/:path*',
+  ],
+};

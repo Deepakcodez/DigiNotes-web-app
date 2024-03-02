c44e229d7aa258dc11a085c0d80a8b657002b4e9
Deepak <deepak2603dk@gmail.com>
2024-03-02 19:43:08 +0530

    home page board design

--- src/app/dbConfig/dbConfig.ts
+++ src/app/dbConfig/dbConfig.ts
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 const mongoURI = process.env.MONGO_URI;
 export async function connect() {
     try {
-        await mongoose.connect(mongoURI!, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await mongoose.connect(mongoURI!, {});
         console.log('Database connected successfully');
     } catch (error) {
         console.error('Error connecting to database:', error);
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,25 +1,36 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import axios from "axios";
+import { Plus } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { Fragment , useRef} from "react";
+import { motion, useTransform, useScroll } from "framer-motion";
+
 export default function Home() {
   const router = useRouter();
+  const card = [1, 1, 1, 1, 1, 1, 1, 1, 1,1,,1,1];
 
-  
   return (
     <>
-    <div className="pt-[3rem] bg-gray-100  h-full ">
-
-      <h1 className="text-5xl mt-[4rem] py-3 ml-[2.5rem]">Boards</h1>
-      <div className="w-full flex justify-center">
-
-      <div className="  h-[70vh] md:h-[55vh]  flex  w-[95%] rounded-2xl  bg-gray-200">
-        <div className="">creater</div>
-        <div>child </div>
+      <div className="pt-[3rem] bg-gray-100  h-full px-2">
+        <div className="text-5xl font-semibold">Boards</div>
+        {/* parent box */}
+        <div className="bg-gray-300 rounded-md h-[70vh] md:h-[55vh] p-5 flex gap-2">
+          <div className="px-[5rem] rounded-lg h-full bg-blue-500 flex justify-center items-center"> <Plus color="white" size={40}/> </div>
+          {/* cards */}
+          <div className="bg-black w-full flex flex-row gap-2 overflow-x-auto no-scrollbar ">
+       
+        
+            {
+              card.map((data, index)=>(
+                <Fragment key={index}>
+                 <div className=" px-[7rem] rounded-md h-full bg-blue-100">{data}</div>
+                </Fragment>
+              ))
+            }
+          </div>
+        </div>
       </div>
-      </div>
-     
-    </div>
     </>
   );
 }

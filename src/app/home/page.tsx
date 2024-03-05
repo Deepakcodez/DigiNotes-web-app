f61e2fc10f221909f39d87484f9c1219dce99a6d
Deepak <deepak2603dk@gmail.com>
2024-03-05 23:01:51 +0530

    creating scrollable card

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,12 +3,22 @@ import { Button } from "@/components/ui/button";
 import axios from "axios";
 import { Plus } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { Fragment , useRef} from "react";
+import { Fragment , useEffect, useRef, useState} from "react";
 import { motion, useTransform, useScroll } from "framer-motion";
+import { HorizontalScrollCarousel } from "@/components/boardcard/boardscroll/page";
+import { number } from "zod";
 
 export default function Home() {
   const router = useRouter();
-  const card = [1, 1, 1, 1, 1, 1, 1, 1, 1,1,,1,1];
+  const card = [1, 1, 1, 1, 1, 1, 1, 1];
+  const [width, setWidth] = useState(0)
+  const carousel = useRef()
+
+
+  useEffect(()=>{
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth  )
+    console.log('>>>>>>>>>>>', carousel.current.scrollWidth - carousel.current.offsetWidth )
+  },[])
 
   return (
     <>
@@ -18,17 +28,20 @@ export default function Home() {
         <div className="bg-gray-300 rounded-md h-[70vh] md:h-[55vh] p-5 flex gap-2">
           <div className="px-[5rem] rounded-lg h-full bg-blue-500 flex justify-center items-center"> <Plus color="white" size={40}/> </div>
           {/* cards */}
-          <div className="bg-black w-full flex flex-row gap-2 overflow-x-auto no-scrollbar ">
-       
-        
+          <motion.div ref={carousel}  className=" w-full flex flex-row gap-2 overflow-hidden">
+          <motion.div
+          drag='x'
+          dragConstraints={{right:0, left:-width}}
+          className="  flex gap-3">
             {
               card.map((data, index)=>(
                 <Fragment key={index}>
-                 <div className=" px-[7rem] rounded-md h-full bg-blue-100">{data}</div>
+                 <div className=" px-[7rem] rounded-md h-full bg-blue-100 cursor-grab">{data}</div>
                 </Fragment>
               ))
-            }
-          </div>
+            } 
+            </motion.div>
+          </motion.div>
         </div>
       </div>
     </>
